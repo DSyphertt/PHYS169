fe567cdab469fb52ce8cc96f9b30d5e44211b2aa
DSyphertt <andresyphertt@brandeis.edu>
2023-10-02 14:57:34 -0400

    Working strip

--- Checker.m
+++ Checker.m
@@ -14,14 +14,8 @@ ylabel('Temperature (°C)');
 title('Temperature Strip Chart');
 grid on;
 
-% Initialize arrays for storing data
-maxPoints = 100; % Maximum number of data points to display
-
-% Specify the number of iterations to run (or run indefinitely)
-numIterations = 100; % Change this to the desired number of iterations
-
-% Read and update the strip chart for a specified number of iterations (or indefinitely)
-for iteration = 1:numIterations
+% Read and update the strip chart indefinitely
+while ishandle(ax)
     % Read data from the serial port
     data = fgetl(ser);
     
@@ -34,20 +28,11 @@ for iteration = 1:numIterations
             % Add data points to the strip chart
             addpoints(stripChart, numericValues(1), numericValues(2));
             
-            % Update the axes limits
-            axis([ax, currentIndex - maxPoints, currentIndex, 0, 40]);
-            
             % Display the received numerical data with labels
             fprintf('Time: %.2f s, Temperature: %.2f °C\n', numericValues(1), numericValues(2));
             
             % Update the plot
             drawnow limitrate;
-            
-            % Increment the index and wrap around if needed
-            currentIndex = currentIndex + 1;
-            if currentIndex > maxPoints
-                currentIndex = 1;
-            end
         else
             % Display the raw data as is
             disp(['Received data: ' data]);
@@ -55,7 +40,7 @@ for iteration = 1:numIterations
     end
 end
 
-% Close the serial port connection when done
+% Close the serial port connection when the figure is closed
 fclose(ser);
 delete(ser);
 clear ser;
